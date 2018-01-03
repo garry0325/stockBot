ad71a48094bd27ca6cf087fe2eab4beb3898f1f7
garry0325 <34297472+garry0325@users.noreply.github.com>
2018-01-03 21:00:27 +0800

    Add files via upload

--- fsm.py
+++ fsm.py
@@ -1,65 +1,10 @@
-import sys
-from io import BytesIO
+from transitions.extensions import GraphMachine
 
-import telegram
-from flask import Flask, request, send_file
-
-from fsm import TocMachine
-
-machine = TocMachine(
-					 states=[
-							 'modeSelection',
-							 'stock',
-							 'currency'
-							 'makeOrder'
-							 ],
-					 transitions=[
-								  {
-								  'trigger': 'stockTermsTriggered',
-								  'source': 'modeSelection',
-								  'dest': 'stock',
-								  
-								  },
-								  {
-								  'trigger': 'currencyTermsTriggered',
-								  'source': 'modeSelection',
-								  'dest': 'currency',
-								  },
-								  {
-								  'trigger': 'currencyTermsTriggered',
-								  'source': 'stock',
-								  'dest': 'currency'
-								  },
-								  {
-								  'trigger': 'stockTermsTriggered',
-								  'source': 'currency',
-								  'dest': 'stock'
-								  },
-								  {
-								  'trigger': 'buy',
-								  'source': 'stock',
-								  'dest': 'order'
-								  },
-								  {
-								  'trigger': 'cancel',
-								  'source': 'order',
-								  'dest': 'stock'
-								  },
-								  {
-								  'trigger': 'orderConfirmed',
-								  'source': 'order',
-								  'dest': 'modeSelection'
-								  }
-								  ],
-					 initial='modeSelection',
-					 auto_transitions=False,
-					 show_conditions=True,
-					 )
+class stockMachine(GraphMachine):
+	def __init__(self, **machine_configs):
+		self.machine=GraphMachine(
+			model=self,
+			**machine_configs
+		)
 
 
-@app.route('/show-fsm', methods=['GET'])
-def show_fsm():
-	byte_io = BytesIO()
-	machine.graph.draw(byte_io, prog='dot', format='png')
-	byte_io.seek(0)
-	return send_file(byte_io, attachment_filename='fsm.png', mimetype='image/png')
